2e1cdd17ae91123ac36b8e5f6d53b1712f91900c
Eicoma <328084834@qq.com>
2021-04-05 19:26:22 +0800

    ...

--- TestDemo_1/src/streamfileio/BufferedReaderChar.java
+++ TestDemo_1/src/streamfileio/BufferedReaderChar.java
@@ -7,7 +7,7 @@ public class BufferedReaderChar {
         char c;
         //使用System.in创建BufferedReader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("输入字符，按下'q'键退出。");
+        System.out.println("输入字符，按下'q'键退出。。");
         //读取字符
         do{
             c = (char)br.read();
